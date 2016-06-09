b57edd94023cf021af360fea1be16a1bbdac4ae2
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-06-09 14:28:01 +0200

    functions: open function — open any file

--- functions.sh
+++ functions.sh
@@ -29,3 +29,89 @@ alias notaf='notify-after'
 
 # urgent flag on the window
 alias bell='echo -e "\a"'
+
+
+# open file without opener specification
+function open() {
+    # Automatically open given input file with the proper program
+    # -s option allow to use the alternative openers for following files
+    # open -s archive.tar.gz                  »»»»   extract
+    # open archive.tar.gz                     »»»»   show file
+    # open archive.tar.gz -s archive.tar.gz   »»»»   show file then extract
+
+    # Declare mapping {extension: opener}
+    declare -A openers
+    VIEWTAR="tar ftv"
+    openers[pdf]="evince"
+    openers[bz]=$VIEWTAR
+    openers[bz2]=$VIEWTAR
+    openers[gz]=$VIEWTAR
+    openers[gz2]=$VIEWTAR
+    openers[lzma]=$VIEWTAR
+    openers[tgz]=$VIEWTAR
+    # images
+    IMAGE_VIEWER="feh"
+    openers[png]=$IMAGE_VIEWER
+    openers[jpg]=$IMAGE_VIEWER
+    openers[gif]=$IMAGE_VIEWER
+    openers[bmp]=$IMAGE_VIEWER
+
+    # Alternatives openers, used when USE_ALT is true
+    USE_ALT=false
+    declare -A alt_openers
+    UNTAR="tar xvf"
+    alt_openers[bz]=$UNTAR
+    alt_openers[bz2]=$UNTAR
+    alt_openers[gz]=$UNTAR
+    alt_openers[gz2]=$UNTAR
+    alt_openers[lzma]=$UNTAR
+    alt_openers[tgz]=$UNTAR
+
+    # In all other cases, use the default openers
+    DEFAULT_OPENER="vim"
+
+    # Declare which openers should be launch asynchronously {openers: async}
+    # spaces between, before and after are necessary.
+    ASYNC=" evince feh "
+
+
+    for filepath in "$@"
+    do
+        echo "filepath: $filepath"
+        # switch alternative use on -a or -s switch
+        if [[ $filepath = "-s" ]] ;
+        then
+            if [ $USE_ALT = true ] ;
+            then
+                USE_ALT=false
+            else
+                USE_ALT=true
+            fi
+            continue
+        fi
+
+        filename=$(basename "$filepath")
+        extension="${filename##*.}"
+
+        # pick the (alternative) opener
+        if [ $USE_ALT = true ] ;
+        then
+            opener="${alt_openers[$extension]}"
+        else
+            opener="${openers[$extension]}"
+        fi
+
+        if [[ $opener && (-f $filepath || -d $filepath)]] ;
+        then
+            command="$opener $filepath"
+            if [[ $ASYNC == *" $opener "* ]] ;
+            then
+                command="$command &"
+            fi
+        else
+            command="$DEFAULT_OPENER $filepath"
+        fi
+        echo $command
+        echo $command | sh
+    done
+}
