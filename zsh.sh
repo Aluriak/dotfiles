0bcd6ff19d7b8204e8fed1fee6dd61bdd006e29e
Aluriak <lucas.bourneuf@laposte.net>
2016-08-22 22:04:46 +0200

    zsh: use . instead of pwd, because errors when special char in path

--- zsh.sh
+++ zsh.sh
@@ -2,7 +2,7 @@
 
 # zsh hook function, called when the working directory change.
 function chpwd() {
-    verbosels --color $(pwd)
+    verbosels --color .
 }
 
 # zsh hook function, called after the command.
