1374c14c481c0ea1c94e648b67a20e5c4cf96e96
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-09-12 13:40:53 +0200

    functions ls_grep get --color and -R natively

--- functions.sh
+++ functions.sh
@@ -23,10 +23,10 @@ function bg() {
 
 # automatic ls pipe grep command
 function ls_grep() {
-    ls $1 | grep "${@:2}"
+    ls -R --color $1 | grep "${@:2}"
 }
 function ll_grep() {
-    ls -l $1 | grep "${@:2}"
+    ls -l -R --color $1 | grep "${@:2}"
 }
 
 # notify-send after given command
