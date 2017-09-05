ca5e72fe5aa0ce64d4391b65ad271a8e3b07285e
Aluriak <lucas.bourneuf@laposte.net>
2017-09-06 01:58:32 +0200

    add-bom function

--- functions.sh
+++ functions.sh
@@ -1,6 +1,33 @@
 # functions repository
 
 
+# add utf8 BOM to given files
+function add-bom() {
+    if [ $# -eq 0 ]
+    then
+            echo usage $0 files ...
+            exit 1
+    fi
+
+    for file in "$@"
+    do
+            echo "# Processing: $file" 1>&2
+            if [ ! -f "$file" ]
+            then
+                    echo Not a file: "$file" 1>&2
+                    exit 1
+            fi
+            TYPE=`file - < "$file" | cut -d: -f2`
+            if echo "$TYPE" | grep -q '(with BOM)'
+            then
+                    echo "# $file already has BOM, skipping." 1>&2
+            else
+                    ( mv "${file}" "${file}"~ && uconv -f utf-8 -t utf-8 --add-signature < "${file}~" > "${file}" ) || ( echo Error processing "$file" 1>&2 ; exit 1)
+            fi
+    done
+}
+
+
 function edit_clipboard() {
     xclip -o -selection clipboard | vipe | xclip -i -selection clipboard
     xclip -o -selection clipboard | xclip -i -selection primary
