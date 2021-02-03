7b6573502d7be3a55e6bad9079e5c205e76a59b7
Lucas Bourneuf <lucas@bourneuf.net>
2021-02-03 10:43:21 +0100

    don't load unused antigen bundle

--- zsh_antigen.sh
+++ zsh_antigen.sh
@@ -15,13 +15,13 @@ antigen bundle git
 antigen bundle pip
 
 # Explain how to install missing binary
-antigen bundle command-not-found
+# antigen bundle command-not-found
 
 # IPFS autocompletion
-antigen bundle aramboi/zsh-ipfs
+# antigen bundle aramboi/zsh-ipfs
 
 # ctrl-z as an alias of fg
-antigen bundle fancy-ctrl-z
+# antigen bundle fancy-ctrl-z
 
 # zsh syntax highlighting
 antigen bundle zsh-users/zsh-syntax-highlighting
