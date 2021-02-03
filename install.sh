6c58611b6ab15b94b9c98f4fb91f9312f516d84e
Lucas Bourneuf <lucas@bourneuf.net>
2021-02-03 10:42:12 +0100

    include bepoz, backgrounds and font

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
 
-path=$(pwd)/
+path=$(pwd)
 
 
 if [ ! -e antigen.zsh ];
@@ -10,11 +10,18 @@ fi
 
 
 # install dotfiles
-ln -s $path.Xdefaults ~/
-ln -s $path.zshrc ~/
-ln -s $path.vimrc.bepo ~/
-ln -s $path.vimrc.bepoz ~/
-ln -s $path.nvimrc ~/.config/nvim/init.vim
+ln -s $path/.Xdefaults ~/
+ln -s $path/.zshrc ~/
+ln -s $path/.vimrc.bepo ~/
+ln -s $path/.vimrc.bepoz ~/
+ln -s $path/.nvimrc ~/.config/nvim/init.vim
+
+# install scripts
+mkdir -p ~/scripts
+for sc in $path/scripts/;
+do
+    ln -s $sc ~/scripts/
+done
 
 # put git aliases
 cat $path.gitconfig >> ~/.gitconfig
---
+++ scripts/switch-background.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# background: never save before shutdown modifications
+# (because a directory have all files exlicited, so modify images in directory don't change feh configuration)
+eval $(cat ~/.fehbg_unchanged)
