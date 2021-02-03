e62ba40545afe343a62b3bdb97e0e75543eee217
Lucas Bourneuf <lucas@bourneuf.net>
2021-02-03 10:25:07 +0100

    installation script + XTERM options

--- Makefile
+++ Makefile
@@ -1,33 +1,9 @@
 all:
 	# do nothing
 
-copy:
-	cp ~/.vimrc ./
-	cp ~/.vimrc.bepo ./
 
-u: update
-update:
-	cp .vimrc ~/.vimrc
-
-dfv:
-	git diff .vimrc
-
-ap:
-	git add -p .vimrc
-
-add:
-	git add .vimrc
-
-addbepo:
-	git add .vimrc.bepo
-
-addall: add addbepo
-
-commit: copy addall
-	git commit
-
-df:
-	vimdiff ~/.vimrc ./.vimrc
+install_dotfiles:
+	./install.sh
 
 
 ################################################################################
--- install.sh
+++ install.sh
@@ -1,32 +1,20 @@
-#########################################
-# DOTFILES
-#########################################
-# Current directory
-DOTDIR=$(dirname $(realpath $0))
-
-# Now create a private file and a specific file if necessary
-touch "$(hostname).sh" privates.sh
-
-# Avoid push of modifications of the private file
-git update-index --assume-unchanged privates.sh
 
 
-#########################################
-# NEOVIM
-#########################################
-# installation for neovim
-mkdir -p ~/.config/nvim
-ln -s "${DOTDIR}"/.nvimrc ~/.config/nvim/init.vim
-
-# installation of neovim's data
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# install all packages
-nvim -c PlugUpdate
+path=$(pwd)/
 
 
-#########################################
-# VIM
-#########################################
-#TODO
+if [ ! -e antigen.zsh ];
+then
+	curl -L git.io/antigen > antigen.zsh
+fi
+
+
+# install dotfiles
+ln -s $path.Xdefaults ~/
+ln -s $path.zshrc ~/
+ln -s $path.vimrc.bepo ~/
+ln -s $path.vimrc.bepoz ~/
+ln -s $path.nvimrc ~/.config/nvim/init.vim
+
+# put git aliases
+cat $path.gitconfig >> ~/.gitconfig
