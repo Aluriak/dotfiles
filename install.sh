cb8490842437c6980cb1bc4b051588f68f7da29b
Lucas Bourneuf <lucas@bourneuf.net>
2021-06-24 14:41:45 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -11,19 +11,22 @@ fi
 
 
 # install dotfiles
-ln -s $hpath/.Xdefaults ~/
-ln -s $hpath/.zshrc ~/
-ln -s $hpath/.vimrc.bepo ~/
-ln -s $hpath/.vimrc.bepoz ~/
-ln -s $hpath/.nvimrc ~/.config/nvim/init.vim
-ln -s $hpath/kitty.conf ~/.config/kitty/
+ln -fs $hpath/.Xdefaults ~/
+ln -fs $hpath/.zshrc ~/
+ln -fs $hpath/.vimrc.bepo ~/
+ln -fs $hpath/.vimrc.bepoz ~/
+ln -fs $hpath/.nvimrc ~/.config/nvim/init.vim
+ln -fs $hpath/kitty.conf ~/.config/kitty/
+ln -fs $hpath/i3/config ~/.i3/
 
 # install scripts
 mkdir -p ~/scripts
-for sc in $hpath/scripts/;
+for sc in $hpath/scripts/*;
 do
-    ln -s $sc ~/scripts/
+    ln -fs $sc ~/scripts/
 done
 
 # put git aliases
 cat $hpath/.gitconfig >> ~/.gitconfig
+
+echo "done!"
