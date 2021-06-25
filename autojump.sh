3dba22bb0daaf14ad3017c6e91e7aea38894daca
Lucas Bourneuf <lucas@bourneuf.net>
2021-06-25 13:54:37 +0200

    just try all autojump locations instead of trying to be smart

--- autojump.sh
+++ autojump.sh
@@ -1,9 +1,10 @@
 
-if [[ $DOTCONF == "arch" ]];
-then
-    source /etc/profile.d/autojump.sh
-elif [[ $DOTCONF == "ubuntu" ]];
-then
-    source /usr/share/autojump/autojump.sh
-fi
 
+# seems to be the arch install dir
+[[ -s /etc/profile.d/autojump.sh ]] && source /etc/profile.d/autojump.sh
+
+# that would be a user-wide one
+[[ -s /home/lucas/.autojump/etc/profile.d/autojump.sh ]] && source /home/lucas/.autojump/etc/profile.d/autojump.sh
+
+# that would be on ubuntu
+[[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
