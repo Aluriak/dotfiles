0c60969108a81cb81709abe314c515571eb9269d
Lucas Bourneuf <lucas.bourneuf@chu-brest.fr>
2025-07-22 14:51:38 +0200

    remove old host

--- hosts/clodomir.sh
+++
@@ -1,2 +0,0 @@
-
-alias crenv3.7='virtualenv venv -p /home/lucas/bin/python3.7/bin/python3.7'
--- hosts/dijkstra.sh
+++
@@ -1,6 +0,0 @@
-export http_proxy=http://localhost:3128
-export HTTP_PROXY=http://localhost:3128
-export ssh_proxy=http://localhost:3128
-export SSH_PROXY=http://localhost:3128
-export ftp_proxy=http://localhost:3128
-export FTP_PROXY=http://localhost:3128
